0905d0263bdb89ed381e9e201f59f3e4c4403a75
Geno M <giantpowpow2@gmail.com>
2019-10-16 08:42:10 +0000

    Automatically backed up by Learn

--- removeDuplicates.js
+++ removeDuplicates.js
@@ -1,6 +1,6 @@
 const numbers = [23, 12, 34, 56, 77, 77, 84, 34, 23, 84, 23, 56, 77, 12, 56, 84, 23, 12, 23, 34, 12, 77, 56];
 const uniques = [];
-for (let i = 0; i <= numbers.length; i++) {
+for (let i = 0; i < numbers.length; i++) {
   if (uniques.includes(numbers[i]) === false) {
     uniques.push(numbers[i]);
   }
